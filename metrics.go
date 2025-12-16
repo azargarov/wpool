cc399b501c894270ee7d650e33b31dc1fee9f00c
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-12-16 15:26:10 +0100

    metrics: remove unnecessary metrics, add returning value for atomic operations

--- metrics.go
+++ metrics.go
@@ -8,10 +8,8 @@ import (
 // Metrics is a snapshot of pool internals useful for monitoring.
 // All fields are read-only through accessor methods.
 type Metrics struct {
-	submitted     atomic.Uint64
 	executed      atomic.Uint64
 	queued        atomic.Int64
-	//maxAge        time.Duration
 	workersActive []atomic.Bool
 }
 
@@ -25,8 +23,13 @@ func (p *Pool[T]) Metrics() *Metrics {
 
 // Executed returns the total number of jobs processed by workers
 // (including canceled ones).
-func (p *Pool[T]) Executed() uint64 { return p.metrics.executed.Load() }
+func (p *Pool[T]) Executed() uint64 { 
+	return p.metrics.executed.Load() 
+}
 
+func (p *Pool[T]) incExecuted() uint64  { 
+	return p.metrics.executed.Add(1) 
+}
 
 // SetWorkerState marks worker id as active/inactive. It is called internally
 // by the pool when workers start or stop.
@@ -45,34 +48,14 @@ func (p *Pool[T]) ActiveWorkers() int {
 	return count
 }
 
-// Submitted returns the total number of jobs accepted by the pool.
-func (p *Pool[T]) Submitted() uint64 { return p.metrics.submitted.Load() }
-
-
-func (p *Pool[T]) incSubmitted() {
-	p.metrics.submitted.Add(1)
+func (p *Pool[T]) Queued() int64 { 
+	return p.metrics.queued.Load() 
 }
 
-//func (p *Pool[T]) incExecuted() {
-//	p.metrics.executed.Add(1)
-//}
-
-//func (p *Pool[T]) setMaxAge(d time.Duration) {
-//	p.metricsMu.Lock()
-//	defer p.metricsMu.Unlock()
-//	p.metrics.maxAge = d
-//}
-
-func (p *Pool[T]) incQueued() {
-	p.metrics.queued.Add(1)
+func (p *Pool[T]) incQueued() int64 {
+	return p.metrics.queued.Add(1)
 }
 
-func (p *Pool[T]) Queued() int64 { return p.metrics.queued.Load() }
-
-func (p *Pool[T]) decQueued() {
-	p.metrics.queued.Add(-1)
-}
-
-func (p *Pool[T]) batchDecQueued(n int64) {
-	p.metrics.queued.Add(-n)
+func (p *Pool[T]) batchDecQueued(n int64) int64{
+	return p.metrics.queued.Add(-n)
 }
