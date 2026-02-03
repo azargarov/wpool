cab20f7a2f943a8b9e29f91a5a367e60912f56c7
Andrey Zargarov <andrey.zargarov@gmail.com>
2026-02-03 21:11:29 +0100

    affinity:doc, add comments

--- affinity.go
+++ affinity.go
@@ -6,6 +6,13 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// PinToCPU pins the current OS thread to a specific CPU.
+//
+// It restricts the calling thread to run only on the given CPU core.
+// This is typically used in conjunction with runtime.LockOSThread
+// to improve cache locality and reduce scheduler-induced migration.
+//
+// This function is Linux-specific and has no effect on other platforms.
 func PinToCPU(cpu int) error {
 	var mask unix.CPUSet
 	mask.Zero()
